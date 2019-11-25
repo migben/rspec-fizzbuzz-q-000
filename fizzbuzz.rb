9408cb94a73e782823b9e5042c902f5e16b56bc1
Miguel Ben <codexheartdevs@gmail.com>
2019-11-25 02:58:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
+  
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
-  elsif num % 3 == 0 && num % 5 == 0
-    return "FizzBuzz"
   else
     return nil
   end
